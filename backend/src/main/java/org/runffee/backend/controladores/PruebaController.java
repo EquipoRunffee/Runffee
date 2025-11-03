ffbd1bbc1d1b56cf6341a9ead0b55e7289c5f9a9
Anderson <anderolivosg@gmail.com>
2025-11-03 12:23:06 +0100

    Permisos de CrossOrigin

--- backend/src/main/java/org/runffee/backend/controladores/PruebaController.java
+++ backend/src/main/java/org/runffee/backend/controladores/PruebaController.java
@@ -3,6 +3,7 @@ package org.runffee.backend.controladores;
 import org.runffee.backend.modelos.Prueba;
 import org.runffee.backend.servicios.PruebaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/prueba")
+@CrossOrigin(origins = {
+        "http://localhost:4200",
+        "https://anderolivos.com/runffee"
+})
 public class PruebaController {
 
     @Autowired
