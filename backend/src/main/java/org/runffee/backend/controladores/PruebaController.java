8d38178c88929a9f3d0b495d7aa3d6226d8b5c11
Anderson <anderolivosg@gmail.com>
2025-11-03 12:34:49 +0100

    CROS fixed

--- backend/src/main/java/org/runffee/backend/controladores/PruebaController.java
+++ backend/src/main/java/org/runffee/backend/controladores/PruebaController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RequestMapping("/prueba")
 @CrossOrigin(origins = {
         "http://localhost:4200",
-        "https://anderolivos.com/runffee"
+        "https://anderolivos.com"
 })
 public class PruebaController {
 
