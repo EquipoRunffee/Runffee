094edb2aeb4f7a51c96bb3005ef98ec57bd1cff8
Valentín Roldán Ojeda <valenroldanojeda@gmail.com>
2025-11-04 21:19:28 +0100

    Modelos (no completado)

--- backend/src/main/java/org/runffee/backend/modelos/Producto.java
+++ backend/src/main/java/org/runffee/backend/modelos/Producto.java
@@ -1,13 +1,40 @@
 package org.runffee.backend.modelos;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.context.annotation.Primary;
 
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 @Entity
+@Table(name = "producto", catalog = "runffee", schema = "app")
+
 public class Producto {
+    //no fk
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
+
+    @Column (name = "nombre")
+    private String nome;
+
+    @Column (name = "imagen")
+    private String imagen;
+
+    @Column (name = "precio")
+    private BigDecimal precio;
+
+    @Column (name = "descripcion")
+    private String descripcion;
+
+    @Column (name = "eliminado")
+    private Boolean eliminado;
 }
--- backend/src/main/java/org/runffee/backend/modelos/Usuario.java
+++ backend/src/main/java/org/runffee/backend/modelos/Usuario.java
@@ -1,13 +1,46 @@
 package org.runffee.backend.modelos;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.context.annotation.Primary;
 
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 @Entity
+@Table(name = "usuario", catalog = "runffee", schema = "app")
+
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id")
     private Integer id;
+
+    @Column (name = "nombre")
+    private String nome;
+
+    @Column (name = "correo")
+    private String correo;
+
+    @Column (name = "contrasena")
+    private String contrasena;
+
+    @Column (name = "ciudad")
+    private String ciudad;
+
+    @Column (name = "pais")
+    private String pais;
+
+    @Column (name = "sexo")
+    private Integer sexo;
+
+    @Column (name = "imagen")
+    private String imagen;
+
+    @Column (name = "eliminado")
+    private Boolean eliminado;
 }
