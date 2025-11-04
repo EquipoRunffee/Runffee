71d7c845324dd17f5892daf1cd4624902344dea3
Anderson <anderolivosg@gmail.com>
2025-11-04 09:52:54 +0100

    Entity declarada en Modelos

--- backend/src/main/java/org/runffee/backend/modelos/Valoracion.java
+++ backend/src/main/java/org/runffee/backend/modelos/Valoracion.java
@@ -1,4 +1,13 @@
 package org.runffee.backend.modelos;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Valoracion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 }
