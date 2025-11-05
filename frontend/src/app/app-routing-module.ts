7bd7d90a410dbcb22ab9a195feb4454a9ef59449
mArteagaRamos <marartram@gmail.com>
2025-11-05 21:33:31 +0100

    añado path de login

--- frontend/src/app/app-routing-module.ts
+++ frontend/src/app/app-routing-module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {Home} from '@nologed/pages/home/home';
 import {Perfil} from '@loged/pages/perfil/perfil';
+import {Login} from '@nologed/pages/login/login';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: Home },
   {path: 'perfil', component: Perfil},
+  {path: 'login', component: Login},
   { path: '**', redirectTo: '/home' },
 ]
 
