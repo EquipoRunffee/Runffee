fcd9f4821c3424a47526cd8e9cab97456b8dcdcc
mArteagaRamos <marartram@gmail.com>
2025-11-06 17:46:24 +0100

    continuo con pagina de login

--- frontend/src/app/features/nologed/pages/login/login.ts
+++ frontend/src/app/features/nologed/pages/login/login.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+    import { Component } from '@angular/core';
+    import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  standalone: false,
   templateUrl: './login.html',
   styleUrl: './login.css',
+  imports: [
+    FormsModule
+  ]
 })
 export class Login {
+
+  usuario = {
+    email: '',
+    password: ''
+  };
+
+  login() {
+    console.log('Credenciales: ', this.usuario);
+  }
 
 }
