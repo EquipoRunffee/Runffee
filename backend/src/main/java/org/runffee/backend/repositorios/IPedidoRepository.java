9f797a0c174d70877f5ac3e1677b7efb69242d15
Anderson <anderolivosg@gmail.com>
2025-11-13 20:51:55 +0100

    Correción media de valoración

--- backend/src/main/java/org/runffee/backend/repositorios/IPedidoRepository.java
+++ backend/src/main/java/org/runffee/backend/repositorios/IPedidoRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface IPedidoRepository extends JpaRepository<Pedido, Integer> {
-    List<Pedido> findBylineaPedidos(LineaPedido lineaPedido);
 }
