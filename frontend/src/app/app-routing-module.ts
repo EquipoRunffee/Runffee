bcc436f60f9d88226d0cecd52f91e4956cc1b840
Valentín Roldán Ojeda <valenroldanojeda@gmail.com>
2025-11-17 20:02:57 +0100

    detallecafeteria 20:02

--- frontend/src/app/app-routing-module.ts
+++ frontend/src/app/app-routing-module.ts
@@ -7,6 +7,7 @@ import {Callback} from '@nologed/pages/callback/callback';
 import {Cafeterias} from '@nologed/pages/cafeterias/cafeterias';
 import { Actividad } from "@loged/components/actividad/actividad";
 import {authGuardGuard} from '@core/guard/auth-guard-guard';
+import {Detallecafeteria} from '@shared/pages/detallecafeteria/detallecafeteria';
 
 const routes: Routes = [
 
@@ -18,20 +19,21 @@ const routes: Routes = [
   {path: 'login', component: Login},
   {path: 'register', component: Register},
   { path: 'actividad', component: Actividad },
-  { path: '**', redirectTo: '/home' },
+  {path: 'detallecafeteria', component: Detallecafeteria},
 
   //PAGINAS LOGED
   {
     path: 'app',
     loadChildren: () =>
       import('@loged/loged-module').then(m => m.LogedModule),
-    canActivate: [authGuardGuard]
+    //canActivate: [authGuardGuard]
   },
   {
     path: 'app/perfil',
     loadChildren: () =>
       import('@loged/pages/perfil/perfil.routes').then(m => m.PERFIL_ROUTES)
   },
+  { path: '**', redirectTo: '/home' },
 ]
 
 @NgModule({
