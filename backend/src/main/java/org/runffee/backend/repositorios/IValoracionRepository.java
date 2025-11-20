e5c7354f116a24f3479e58e9249150b66b901136
IvánPonti <ivan.ponti.rubio06@gmail.com>
2025-11-20 17:43:40 +0100

    correccion errores

--- backend/src/main/java/org/runffee/backend/repositorios/IValoracionRepository.java
+++ backend/src/main/java/org/runffee/backend/repositorios/IValoracionRepository.java
@@ -25,17 +25,17 @@ public interface IValoracionRepository extends JpaRepository<Valoracion, Integer
     BigDecimal obtenerMediaValoracionCafeteria(@Param("cafeteriaId") Integer cafeteriaId);
 
     @Query(value = """
-       SELECT v.*, c.nombre
-       FROM valoracion v
-       JOIN pedido pe ON pe.id_valoracion = v.id
-       JOIN entrenamiento en ON en.id_pedido = pe.id
-       JOIN usuario u ON u.id = en.id_usuario
-       JOIN lineapedido l ON l.id_pedido = pe.id
-       JOIN producto p ON p.id = l.id_producto
-       JOIN cafeteria c ON c.id = p.id_cafeteria
-       WHERE u.id = idUsuario;
-        """,
-            nativeQuery = true)
+        SELECT v.*, c.nombre
+        FROM valoracion v
+        JOIN pedido pe ON pe.id_valoracion = v.id
+        JOIN entrenamiento en ON en.id_pedido = pe.id
+        JOIN usuario u ON u.id = en.id_usuario
+        JOIN lineapedido l ON l.id_pedido = pe.id
+        JOIN producto p ON p.id = l.id_producto
+        JOIN cafeteria c ON c.id = p.id_cafeteria
+        WHERE u.id = :idUsuario
+    """, nativeQuery = true)
+
     List<Object[]> obtenerValoracionEntrenamiento(@Param("idUsuario") Integer idUsuario);
 
 }
