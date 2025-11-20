2407bef2677fcf3b0187266ba4abc31763c6f1cb
IvánPonti <ivan.ponti.rubio06@gmail.com>
2025-11-20 19:24:06 +0100

    valoracionCard

---
+++ frontend/src/app/shared/components/valoracion-card/valoracion-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-valoracion-card',
+  standalone: false,
+  templateUrl: './valoracion-card.html',
+  styleUrl: './valoracion-card.css',
+})
+export class ValoracionCard {
+
+}
