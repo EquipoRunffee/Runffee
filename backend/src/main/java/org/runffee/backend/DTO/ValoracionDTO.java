cae44ededec063141b825a1b85e52720dcac7ad9
IvánPonti <ivan.ponti.rubio06@gmail.com>
2025-11-25 21:03:30 +0100

    diseño carrito

--- backend/src/main/java/org/runffee/backend/DTO/ValoracionDTO.java
+++ backend/src/main/java/org/runffee/backend/DTO/ValoracionDTO.java
@@ -1,6 +1,7 @@
 package org.runffee.backend.DTO;
 
 import lombok.Data;
+import org.runffee.backend.modelos.Valoracion;
 
 import java.math.BigDecimal;
 
@@ -14,4 +15,12 @@ public class ValoracionDTO {
     private String descripcion;
     private Boolean eliminado;
     private String nombreCafeteria;
+
+    public ValoracionDTO(String titulo, BigDecimal cantidad, String descripcion, Boolean eliminado, String nombreCafeteria) {
+        this.titulo = titulo;
+        this.cantidad = cantidad;
+        this.descripcion = descripcion;
+        this.eliminado = eliminado;
+        this.nombreCafeteria = nombreCafeteria;
+    }
 }
