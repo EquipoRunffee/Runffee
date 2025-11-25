329314f7899f881a8b6ef9d66914de573f761df3
Anderson <anderolivosg@gmail.com>
2025-11-25 14:06:45 +0100

    Skeleton loading added

--- frontend/src/app/shared/components/cafeteriaCard/cafeteriaCard.ts
+++ frontend/src/app/shared/components/cafeteriaCard/cafeteriaCard.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {CafeteriaDetalles} from '@core/models/cafeteria-detalles';
 import {CafeteriaDetallesService} from '@core/services/cafeteria/cafeteriaDetallesService';
@@ -13,13 +13,13 @@ import {RouterLink} from '@angular/router';
 })
 
 export class CafeteriaCard implements OnInit{
+  isCafeteriasLoaded: boolean = false;
   cafeterias: CafeteriaDetalles[] = [];
   // Definimos los valores que tiene el objeto, la key(tipoCafeteria es obligatoria un string mientras
   // los valores pueden ser cualquiera)
   cafeteriaPorTipo: { [key: string]: any[] } = {};
 
   @ViewChild('hostCard') hostCard!: ElementRef;
-
 
   constructor(private cafeteriaDetalleService: CafeteriaDetallesService) {}
 
@@ -38,19 +38,12 @@ export class CafeteriaCard implements OnInit{
         this.cafeterias = data;
         // Añadimos la nueva funcion
         this.filtrarCafeteriasPorTipo();
-        console.log('Datos Recibidos:', data);
-        console.log('Cafeterías por tipo:', this.cafeteriaPorTipo);
+        this.isCafeteriasLoaded = true;
       },
       error: (err) => {
         console.error('Error al obtener cafeterías:', err);
       }
     });
-  }
-
-  capitalizeTipo(tipoCafeteria?: string): string {
-    if (!tipoCafeteria) return '';
-    if (tipoCafeteria === 'CAFETERIA') tipoCafeteria = 'CAFETERÍAS';
-    return tipoCafeteria.charAt(0).toUpperCase() + tipoCafeteria.slice(1).toLowerCase();
   }
 
   filtrarCafeteriasPorTipo() {
