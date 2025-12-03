c9ada358afcecc13646330d9044f0aeb46be37b5
Valentín Roldán Ojeda <valenroldanojeda@gmail.com>
2025-12-03 20:54:38 +0100

    ROUTING

--- frontend/src/app/features/admin/controlcafeteria/controlcafeteria.ts
+++ frontend/src/app/features/admin/controlcafeteria/controlcafeteria.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CafeteriaService} from '@core/services/cafeteria/cafeteriaService';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-controlcafeteria',
@@ -6,6 +8,21 @@ import { Component } from '@angular/core';
   templateUrl: './controlcafeteria.html',
   styleUrl: './controlcafeteria.css',
 })
-export class Controlcafeteria {
+export class Controlcafeteria implements OnInit {
+
+    datos:any = null;
+    constructor(private cafeteriaService: CafeteriaService,http: HttpClient) {}
+
+    ngOnInit() {
+      this.cafeteriaService.getCafeteriaPorId(1).subscribe({
+        next: data => {
+          console.log(data);
+          this.datos = data;
+        },
+        error: error => {
+          console.log(error);
+        }
+      })
+    }
 
 }
